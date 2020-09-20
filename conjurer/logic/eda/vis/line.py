32944c5bfd015673e269c146c8b004691fee074b
Not So Fat <yusuke.muraoka.0208@gmail.com>
2020-09-20 12:20:42 -0700

    fix bug on plot_line

--- conjurer/logic/eda/vis/line.py
+++ conjurer/logic/eda/vis/line.py
@@ -5,7 +5,7 @@ def plot_line(df, column_x, column_y, column_yerror=None):
     encode_args = dict(x=column_x, y=column_y)
     if column_yerror:
         encode_args["yError"] = column_yerror
-    base = alt.Chart(df).encode()
+    base = alt.Chart(df).encode(**encode_args)
     c1 = base.mark_line()
     if column_yerror:
         c2 = base.mark_errorband()
